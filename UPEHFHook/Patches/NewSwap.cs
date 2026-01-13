b19ed73c661275935882baf088b36345d1855b33
Ex0du5-2169 <ex0du5.2169@gmail.com>
2026-01-13 23:46:33 +0000

    Forgot to init swapped skeleton data assets.

--- UPEHFHook/Patches/NewSwap.cs
+++ UPEHFHook/Patches/NewSwap.cs
@@ -25,48 +25,56 @@ namespace UPEHFHook.Patches
                     newskel.skeletonDataAsset = UPEHFBase.Reika;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
                 case 6:
                     newskel = __instance.anim;
                     newskel.skeletonDataAsset = UPEHFBase.Nami;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
                 case 90:
                     newskel = __instance.anim;
                     newskel.skeletonDataAsset = UPEHFBase.ESis;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
                 case 110:
                     newskel = __instance.anim;
                     newskel.skeletonDataAsset = UPEHFBase.Giant;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
                 case 113:
                     newskel = __instance.anim;
                     newskel.skeletonDataAsset = UPEHFBase.Cassie;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
                 case 114:
                     newskel = __instance.anim;
                     newskel.skeletonDataAsset = UPEHFBase.Shino;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
                 case 115:
                     newskel = __instance.anim;
                     newskel.skeletonDataAsset = UPEHFBase.Sally;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
                 case 116:
                     newskel = __instance.anim;
                     newskel.skeletonDataAsset = UPEHFBase.Merry;
                     __instance.anim = newskel;
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
+                    __instance.anim.Initialize(true);
                     break;
             }
         }
