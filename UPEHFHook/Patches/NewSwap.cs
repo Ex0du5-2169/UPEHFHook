93008ec6dfb6fed0e66daeb404c76c07363376b4
Ex0du5-2169 <ex0du5.2169@gmail.com>
2026-01-14 17:44:12 +0000

    Set initialSkinName

--- UPEHFHook/Patches/NewSwap.cs
+++ UPEHFHook/Patches/NewSwap.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using HarmonyLib;
 using Spine.Unity;
 using YotanModCore;
+using UnityEngine;
 
 namespace UPEHFHook.Patches
 {
@@ -17,64 +18,81 @@ namespace UPEHFHook.Patches
 
         public static void CSPostFix(CommonStates __instance)
         {
-            SkeletonAnimation newskel = new SkeletonAnimation();
+            SkeletonGraphic skelgraphic = new SkeletonGraphic();
+            //GameObject newobj = __instance.gameObject;
             switch (__instance.npcID)
             {
                 case 5:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.Reika;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.Reika;
+                    skelgraphic.initialSkinName = "default"; //nullref, needs name assigning within Spine?
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
                 case 6:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.Nami;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.Nami;
+                    skelgraphic.initialSkinName = "default";
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
                 case 90:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.ESis;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.ESis;
+                    skelgraphic.initialSkinName = "default";
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
                 case 110:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.Giant;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.Giant;
+                    skelgraphic.initialSkinName = "default";
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
                 case 113:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.Cassie;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.Cassie;
+                    skelgraphic.initialSkinName = "default";
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
                 case 114:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.Shino;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.Shino;
+                    skelgraphic.initialSkinName = "default";
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
                 case 115:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.Sally;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.Sally;
+                    skelgraphic.initialSkinName = "default";
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
                 case 116:
-                    newskel = __instance.anim;
-                    newskel.skeletonDataAsset = UPEHFBase.Merry;
-                    __instance.anim = newskel;
+                    skelgraphic = __instance.GetComponent<SkeletonGraphic>();
+                    skelgraphic.skeletonDataAsset = UPEHFBase.Merry;
+                    skelgraphic.initialSkinName = "default";
+                    skelgraphic.Initialize(true);
                     UPEHFBase.Log.LogInfo("Swapping data asset for " + __instance.npcID);
-                    __instance.anim.Initialize(true);
+                    skelgraphic.AnimationState.SetAnimation(0, "A_Idle", true);
+                    skelgraphic.SetMaterialDirty();
                     break;
             }
         }
