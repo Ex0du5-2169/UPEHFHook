6379d69d34394ca05ce054d666bb631de9418f72
Ex0du5-2169 <ex0du5.2169@gmail.com>
2026-02-07 12:40:55 +0000

    Oopsie fixed

--- UPEHFHook/Patches/AssetsLoader.cs
+++ UPEHFHook/Patches/AssetsLoader.cs
@@ -1,5 +1,9 @@
-﻿using HarmonyLib;
+﻿using BepInEx;
+using HarmonyLib;
+using HFramework;
 using Spine.Unity;
+using System;
+using System.IO;
 using UnityEngine;
 
 namespace UPEHFHook.Patches
@@ -8,44 +12,58 @@ namespace UPEHFHook.Patches
     {
         private static bool loadedAssets = false;
 
-        [HarmonyPatch(typeof(SaveManager), nameof(SaveManager.LoadDLC))]
-        [HarmonyPostfix]
-        private static void LoadAssets()
+        [HarmonyPatch(typeof(SaveManager), "LoadDLC"), HarmonyPostfix]
+        private static void PatchSkeletons(NPCManager __instance)
         {
-            NPCManager npcm = GameObject.Find("NPCManager").GetComponent<NPCManager>();
-            if (UPEHFBase.skelDataAssets == null)
-                UPEHFBase.skelDataAssets = new SkeletonDataAsset[npcm.npcPrefab.Length];
-            // Load and/or replace Assets
-            for (int i = 0; i < npcm.npcPrefab.Length; i++)
+
+            for (int i = 0; i < __instance.npcPrefab.Length; i++)
             {
-                GameObject prefab = npcm.npcPrefab[i];
-
-                if (prefab == null)
-                    continue;
-
-                if (!loadedAssets)
+                GameObject npcPrefab = __instance.npcPrefab[i];
+                if (npcPrefab == null) continue;
+                string name = npcPrefab.name.Replace("_prefab", "");
+                string npcPrefabPath = Paths.ConfigPath + "/skeletonReplacers/" + name;
+                if (!Directory.Exists(npcPrefabPath))
                 {
-                    string assetName = prefab.name.Replace("_prefab", "_SkeletonData.asset");
-                    UPEHFBase.skelDataAssets[i] = UPEHFBase.Assets.LoadAsset<SkeletonDataAsset>(assetName);
-                }
-                SkeletonDataAsset newDataAsset = UPEHFBase.skelDataAssets[i];
-                if (newDataAsset == null)
+                    UPEHFBase.Log.LogInfo(npcPrefabPath + " skipped");
                     continue;
-
-                SkeletonAnimation anim = prefab.GetComponentInChildren<SkeletonAnimation>();
-
-                anim.skeletonDataAsset = newDataAsset;
-                anim.initialSkinName = "default";
-                anim.Initialize(true);
-                if (anim.skeleton.FindSlot("Body_preg") != null)
-                {
-                    anim.skeleton.SetAttachment("Body_preg", null);
                 }
-                SkeletonGraphic graph = anim.GetComponent<SkeletonGraphic>();
-                graph.SetMaterialDirty();
-                UPEHFBase.Log.LogInfo("Patched " + prefab.name + "(npcID [" + i + "])");
+
+                try
+                {
+                    SkeletonAnimation prefabAnimation = npcPrefab.GetComponentInChildren<SkeletonAnimation>();
+
+                    byte[] textureData = File.ReadAllBytes(npcPrefabPath + "/" + name + ".png");
+                    Texture2D texture = new Texture2D(2, 2, TextureFormat.RGBA32, false, false);
+                    texture.LoadImage(textureData, false);
+                    texture = WorkshopNPCManager.PadToMultipleOf4_CPU(texture, true);
+                    texture.wrapMode = TextureWrapMode.Clamp;
+                    texture.filterMode = FilterMode.Bilinear;
+                    texture.anisoLevel = 1;
+                    texture.Apply(true, false);
+                    texture.Compress(true);
+                    texture.Apply(false, true);
+                    texture.name = name;
+
+                    TextAsset skeletonDataFile = new TextAsset(File.ReadAllText(npcPrefabPath + "/" + name + ".json"));
+                    TextAsset atlasText = new TextAsset(File.ReadAllText(npcPrefabPath + "/" + name + ".atlas"));
+                    Material mat = UnityEngine.Object.Instantiate<Material>(prefabAnimation.GetComponent<MeshRenderer>().materials[0]);
+                    SpineAtlasAsset atlasAsset = SpineAtlasAsset.CreateRuntimeInstance(atlasText, new Texture2D[] { texture }, mat, true);
+                    SkeletonDataAsset skeletonDataAsset = SkeletonDataAsset.CreateRuntimeInstance(skeletonDataFile, atlasAsset, true, 0.01f);
+                    prefabAnimation.skeletonDataAsset = skeletonDataAsset;
+                    prefabAnimation.Initialize(true);
+                    prefabAnimation.skeleton.SetAttachment("Body_preg", null);
+                    if (prefabAnimation.skeleton.FindSlot("Body_Cloth") != null)
+                        prefabAnimation.skeleton.FindSlot("Body_Cloth").SetColor(new Color(1f, 1f, 1f, 1f));
+                    prefabAnimation.state.SetAnimation(0, "A_idle", true);
+                    UPEHFBase.Log.LogWarning("Replaced " + i + " skeleton.");
+                }
+                catch (Exception ex)
+                {
+                    UPEHFBase.Log.LogError("Failed to replace npc with id " + i);
+                    UPEHFBase.Log.LogWarning(ex.Message);
+                }
+
             }
-            loadedAssets = true;
         }
     }
 }
